2964f718bc27d5058f0bc0444c208e92ac4676f0
NathanMad <nathan@internit.com.br>
2023-11-06 11:59:53 -0300

    Revert "refactor"
    This reverts commit f2bb725ef6a72506565d09ffee96e8624bb496f2.

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
-			for (let el of document.querySelectorAll('img, iframe')) {
-				let coords = el.getBoundingClientRect();
-				el.setAttribute('data-src', el.src);
-				el.removeAttribute('src');
-				if (((coords.y >= 0 && coords.y <= window.innerHeight) || (coords.bottom >= 0 && coords.bottom <= window.innerHeight))) {
-					el.src = el.dataset.src;
-				};
-			};
-		});
-		window.addEventListener('scroll', () => {
-			for (let el of document.querySelectorAll('img, iframe')) {
-				let coords = el.getBoundingClientRect();
-				if (((coords.y >= 0 && coords.y <= window.innerHeight) || (coords.bottom >= 0 && coords.bottom <= window.innerHeight))) {
-					el.src = el.dataset.src;
-				};
-			};
-		});
+    for (let el of document.querySelectorAll('img, iframe')) {
+        let coords = el.getBoundingClientRect();
+        el.setAttribute('data-src', el.src);
+        el.removeAttribute('src');
+        if (((coords.y >= 0 && coords.y <= window.innerHeight) || (coords.bottom >= 0 && coords.bottom <= window.innerHeight))) {
+            el.src = el.dataset.src;
+        };
+    };
+});
+window.addEventListener('scroll', () => {
+    for (let el of document.querySelectorAll('img, iframe')) {
+        let coords = el.getBoundingClientRect();
+        el.setAttribute('data-src', el.src);
+        el.removeAttribute('src');
+        if (((coords.y >= 0 && coords.y <= window.innerHeight) || (coords.bottom >= 0 && coords.bottom <= window.innerHeight))) {
+            el.src = el.dataset.src;
+        };
+    };
+});
